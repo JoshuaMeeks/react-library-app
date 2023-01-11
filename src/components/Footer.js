487fc554a9b198fec106c18166c3991630c535e0
Your Name <jlmeeks587@gmail.com>
2023-01-11 10:59:29 -0500

    Initial files and header/footer completed

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <footer>
+      Copyright &copy; 2023 Joshua Meeks
+    </footer>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className='logo'>
+        <span className='logo-lettering'>Library</span>
+      </div>
+    </header>
+  )
+  }
