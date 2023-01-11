d76ea7afd09a7cc2d0a3c3aa72f86a8eb0d7c427
Your Name <jlmeeks587@gmail.com>
2023-01-11 11:11:01 -0500

    Add book button added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { AddBookButton } from './components/AddBookButton';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddBookButton />
       <Footer />
     </div>
   );
---
+++ src/components/AddBookButton.js
@@ -0,0 +1,9 @@
+import './AddBookButton.css';
+
+export const AddBookButton = () => {
+  return (
+    <div class="button-container">
+      <button class="add-book-btn">Add a book</button>
+    </div>
+  )
+}
