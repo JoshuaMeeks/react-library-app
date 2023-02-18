56650a57dc13facd5e05575ce64515e231c0aad2
Your Name <jlmeeks587@gmail.com>
2023-02-17 22:23:07 -0500

    Added state for read toggle button in book card

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ function App() {
   const [library, setLibrary] = useState([]);
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [bookModal, setBookModal] = useState(false);
+  const [readStatus, setReadStatus] = useState(false);
   
   const toggleBookModal = () => {
     setBookModal(state => !state);
   };
-  const [bookModal, setBookModal] = useState(false);
   
   const handleSubmit = (e) => {
     e.preventDefault();
